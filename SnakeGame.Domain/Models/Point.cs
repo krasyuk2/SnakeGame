e5fd083101f43283c02505c5a08f1be8e9c9165e
krasyuk2 <krasyuk.maksimka@gmail.com>
2026-06-30 23:12:17 +0700

    init

---
+++ SnakeGame.Domain/Models/Point.cs
@@ -0,0 +1,32 @@
+﻿namespace SnakeGame.Domain.Models;
+
+/// <summary>
+///     Модель позиции.
+/// </summary>
+public struct Point
+{
+    /// <summary>
+    ///     Координата по X.
+    /// </summary>
+    public int X { get; set; }
+    
+    /// <summary>
+    ///     Координата по Y.
+    /// </summary>
+    public int Y { get; set; }
+    
+    /// <summary>
+    ///     Символ отображения.
+    /// </summary>
+    public char Symbol { get; set; }
+    
+    /// <summary>
+    ///     Конструктор.
+    /// </summary>
+    public Point(int x, int y, char symbol)
+    {
+        X = x;
+        Y = y;
+        Symbol = symbol;
+    }
+}
